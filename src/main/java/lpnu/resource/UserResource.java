e8699fb4360e568f327ceb2b35769e95336a2fd8
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2022-11-19 18:40:12 +0200

    update1

--- src/main/java/lpnu/resource/UserResource.java
+++ src/main/java/lpnu/resource/UserResource.java
@@ -2,6 +2,7 @@ package lpnu.resource;
 
 import lpnu.dto.UserDTO;
 import lpnu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class UserResource {
-
+    @Autowired
     private UserService userService;
 
     @GetMapping("/users")
