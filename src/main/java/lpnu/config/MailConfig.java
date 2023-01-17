9c0e460b17863e0920b76909152b792b00d7773a
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2023-01-17 16:53:40 +0200

    for privacy

--- src/main/java/lpnu/config/MailConfig.java
+++ src/main/java/lpnu/config/MailConfig.java
@@ -1,6 +1,5 @@
 package lpnu.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,31 +10,13 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.username}")
-//    private String username;
-//
-//    @Value("${spring.mail.password}")
-//    private String password;
-//
-//    @Value("${spring.mail.port}")
-//    private int port;
-//
-//    @Value("${spring.mail.protocol}")
-//    private String protocol;
-//
-//    @Value("${mail.debug}")
-//    private String debug;
-
     @Bean
     public JavaMailSender getMailSender() {
         final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setUsername("vasyl.nul@gmail.com");
-        mailSender.setPassword("ktygbxlgrkejdjhj");
+        mailSender.setUsername("username");
+        mailSender.setPassword("password");
         mailSender.setPort(587);
 
         final Properties props = mailSender.getJavaMailProperties();
